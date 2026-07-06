601ac774416bf1ef6afe711a9d70b203d8e4df4e
AminaUmerr <aminaumer17@gmail.com>
2026-07-06 18:32:15 +0500

    Add SQL queries for student data retrieval

--- Week-1/Day-1/SQL_Practice.sql
+++ Week-1/Day-1/SQL_Practice.sql
@@ -1,1 +1,21 @@
+-- Display all students
+SELECT *
+FROM Students;
 
+-- Display only student names
+SELECT Name
+FROM Students;
+
+-- Students older than 21
+SELECT *
+FROM Students
+WHERE Age > 21;
+
+-- Sort students by age
+SELECT *
+FROM Students
+ORDER BY Age DESC;
+
+-- First 3 students
+SELECT TOP 3 *
+FROM Students;
